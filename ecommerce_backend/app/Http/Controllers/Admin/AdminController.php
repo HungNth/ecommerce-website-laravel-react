72ccd925606f6def7cfe30e19cd66d5e84b6ffc2
HungNth <thienhungnth@gmail.com>
2026-02-24 15:06:37 +0700

    feat: backend - add AdminController with order stats, authentication, and session handling logic

---
+++ ecommerce_backend/app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthAdminRequest;
+use App\Models\Order;
+use Carbon\Carbon;
+
+class AdminController extends Controller
+{
+    /**
+     * Fetch and display today, yesterday, this month, this year orders
+     */
+    public function index()
+    {
+        // get orders
+        $todayOrders = Order::whereDay('created_at', Carbon::today())->get();
+        $yesterdayOrders = Order::whereDay('created_at', Carbon::yesterday())->get();
+        $monthOrders = Order::whereMonth('created_at', Carbon::now()->month)->get();
+        $yearOrders = Order::whereYear('created_at', Carbon::now()->year)->get();
+        
+        return view('admin.index', [
+            'todayOrders' => $todayOrders,
+            'yesterdayOrders' => $yesterdayOrders,
+            'monthOrders' => $monthOrders,
+            'yearOrders' => $yearOrders,
+        ]);
+    }
+    
+    /**
+     * Display the login form
+     */
+    public function login()
+    {
+        if (!auth()->guard('admin')->check()) {
+            return view('admin.login');
+        }
+        
+        return redirect()->route('admin.index');
+    }
+    
+    /**
+     * Auth the admin
+     */
+    public function auth(AuthAdminRequest $request)
+    {
+        if ($request->validated()) {
+            if (!auth()->guard('admin')->attempt([
+                'email' => $request->email,
+                'password' => $request->password,
+            ])) {
+                $request->session()->regenerate();
+                return redirect()->route('admin.index');
+            }
+        }
+        
+        return redirect()->route('admin.login')
+            ->with('error', 'These credentials do not match our records');
+    }
+    
+    /**
+     * Log out the admin
+     */
+    public function logout()
+    {
+        auth()->guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+}
