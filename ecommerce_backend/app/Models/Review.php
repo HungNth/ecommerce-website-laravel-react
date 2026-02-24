ce5dd19842e6baabf97cf5493da280dc4c95a338
HungNth <thienhungnth@gmail.com>
2026-02-24 14:16:03 +0700

    feat: backend - enhance Review model with fillable attributes, relationships, and custom accessor

--- ecommerce_backend/app/Models/Review.php
+++ ecommerce_backend/app/Models/Review.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'body', 'user_id', 'approved', 'rating', 'product_id',
+    ];
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+    
+    public function getCreatedAtAttribute($value): string
+    {
+        return Carbon::parse($value)->diffForHumans();
+    }
 }
