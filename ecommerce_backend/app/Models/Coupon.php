49e6b3c23b96a0be2dc9134b1280983573f4ab65
HungNth <thienhungnth@gmail.com>
2026-02-24 14:15:37 +0700

    feat: backend - enhance Coupon model with fillable attributes and validation logic

--- ecommerce_backend/app/Models/Coupon.php
+++ ecommerce_backend/app/Models/Coupon.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Coupon extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'discount',
+        'valid_until',
+    ];
+    
+    /**
+     * Convert the coupon name to uppercase
+     */
+    public function setNameAttribute($value): void
+    {
+        $this->attributes['name'] = Str::upper($value);
+    }
+    
+    /**
+     * Check if a coupon is valid
+     */
+    public function checkIfValid(): bool
+    {
+        if ($this->valid_until > Carbon::now()) {
+            return true;
+        }
+        
+        return false;
+    }
 }
