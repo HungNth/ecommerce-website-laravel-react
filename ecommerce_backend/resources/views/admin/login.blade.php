45ca9b4bd3fa2d7a91561d5e7b4dfa9f5a2c6b46
HungNth <thienhungnth@gmail.com>
2026-02-24 16:41:12 +0700

    feat: backend - add admin panel views including layout, login, and dashboard pages

---
+++ ecommerce_backend/resources/views/admin/login.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body class="bg-light">
+
+<div class="container">
+    <div class="row my-5">
+        <div class="col-md-6 mx-auto">
+            @session('error')
+            <div class="alert alert-danger my-2">
+                {{ session('error') }}
+            </div>
+            @endsession()
+            <div class="card shadow-sm p-5">
+                <div class="card-header bg-white text-center">
+                    <h3 class="mt-2">Login</h3>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('admin.auth') }}" method="POST">
+                        @csrf
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                                   name="email"
+                                   placeholder="name@example.com">
+                            <label for="email">Email address*</label>
+                            @error('email')
+                            <span class="invalid-feedback">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                   id="password" name="password"
+                                   placeholder="Password">
+                            <label for="password">Password*</label>
+                            @error('password')
+                            <span class="invalid-feedback">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                        <div class="mb-2">
+                            <button class="btn btn btn-dark" type="submit">Sign in</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+</html>
