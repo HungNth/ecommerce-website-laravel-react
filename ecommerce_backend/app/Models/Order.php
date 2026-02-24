68368f6bc36a5d4a48c6b883b5bcfa2b2bc356b7
HungNth <thienhungnth@gmail.com>
2026-02-24 14:15:48 +0700

    feat: backend - enhance Order model with fillable attributes and relationships

--- ecommerce_backend/app/Models/Order.php
+++ ecommerce_backend/app/Models/Order.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['qty', 'total', 'delivered_at', 'user_id', 'coupon_id'];
+    
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function coupon(): BelongsTo
+    {
+        return $this->belongsTo(Coupon::class);
+    }
 }
