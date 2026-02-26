304372c98d4de9f60855ca517c990851e5d0398c
HungNth <thienhungnth@gmail.com>
2026-02-26 16:39:20 +0700

    refactor: frontend - remove default styles and templates, integrate routing, and update app entry styling

--- ecommerce_frontend/src/main.jsx
+++ ecommerce_frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
+
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="container card shadow-sm my-4">
+        <ToastContainer position="top-right" />
+        <App />
+    </div>,
+);
